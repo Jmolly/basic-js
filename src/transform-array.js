a180d58a55b2e1879b3f916759ef47f5e080c3f7
jmolly <roshkovskypetr@gmail.com>
2020-10-12 11:27:20 +0300

    feat: add solution to hanoi-tower, transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw e;
+  let result = [];
+  let skipNext = false;
+
+  arr.forEach((el, index) => {
+    if (skipNext) {
+      skipNext = false;
+    } else {
+      switch (el) {
+        case "--discard-next":
+          result = [...result, undefined, undefined];
+          skipNext = true;
+          break;
+
+        case "--discard-prev":
+          result[result.length - 1] = undefined;
+          result = [...result, undefined];
+          break;
+
+        case "--double-next":
+          result = [...result, arr[index + 1]];
+          break;
+
+        case "--double-prev":
+          result = [...result, result[result.length - 1]];
+          break;
+
+        default:
+          result = [...result, el];
+      }
+    }
+  });
+  return result.filter((el) => el !== undefined);
 };
