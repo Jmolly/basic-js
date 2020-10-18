2463c4665771a6a87aaacd52efd760a5cf138dde
jmolly <roshkovskypetr@gmail.com>
2020-10-19 00:46:27 +0300

    fix: fix carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,12 +3,14 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(sampleActivity) {
-  const isValid = typeof sample === "string" && typeof +sample !== "number";
+module.exports = function dateSample(sample) {
+  const isValid =
+    typeof sample === "string" &&
+    typeof +sample === "number" &&
+    +sample > 0 &&
+    sample < 15;
   const result = isValid
-    ? Math.ceil(
-        Math.log2(MODERN_ACTIVITY / +sampleActivity) / 0.693 / HALF_LIFE_PERIOD
-      )
+    ? Math.ceil(Math.log(MODERN_ACTIVITY / sample) / (0.693 / HALF_LIFE_PERIOD))
     : false;
 
   return result;
