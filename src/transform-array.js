5c43088bcfa09c6d2a2f132a63d76b5e45c81742
jmolly <roshkovskypetr@gmail.com>
2020-10-19 01:25:44 +0300

    test

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,7 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  throw new CustomError("Not implemented");
   if (!Array.isArray(arr)) throw e;
   let result = [];
   let skipNext = false;
