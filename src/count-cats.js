0334275e73cc948d5deddc43f9750be720c6f73e
jmolly <roshkovskypetr@gmail.com>
2020-10-19 01:12:44 +0300

    test

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
+  throw new CustomError("Not implemented");
   let result = 0;
 
   matrix.forEach((element) => {
