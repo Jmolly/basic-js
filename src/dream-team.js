1cdc809520d787ff048f8ef7a347643bb0671b8f
jmolly <roshkovskypetr@gmail.com>
2020-10-18 20:51:46 +0300

    fix: fix dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -3,9 +3,13 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function createDreamTeam(members) {
   let result = [];
 
-  members.forEach((element) => {
-    result = typeof element === "string" ? [...result, element[0]] : result;
-  });
+  Array.isArray(members) &&
+    members.forEach((element) => {
+      result =
+        typeof element === "string"
+          ? [...result, element.trim()[0].toUpperCase()]
+          : result;
+    });
 
   return result.sort().join("");
 };
