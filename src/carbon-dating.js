df1863d50c194e5f7d33bcf40ed0803667fd9b2e
jmolly <roshkovskypetr@gmail.com>
2020-10-19 00:53:08 +0300

    fix: fix carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -8,11 +8,9 @@ module.exports = function dateSample(sample) {
     typeof sample === "string" &&
     typeof +sample === "number" &&
     +sample > 0 &&
-    sample < 15;
+    +sample < 15;
   const result = isValid
-    ? Math.ceil(
-        Math.log2(MODERN_ACTIVITY / sample) / (0.693 / HALF_LIFE_PERIOD)
-      )
+    ? Math.ceil(Math.log(MODERN_ACTIVITY / sample) / (0.693 / HALF_LIFE_PERIOD))
     : false;
 
   return result;
