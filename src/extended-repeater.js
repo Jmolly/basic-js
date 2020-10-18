901c7243b7371303aa3992c9dafb56428b1ffc60
jmolly <roshkovskypetr@gmail.com>
2020-10-18 19:58:53 +0300

    fix: fix repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -3,13 +3,14 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function repeater(str, options) {
   const isUndef = (val) => (val === undefined ? "" : val);
 
-  const additionArr = (len) =>
-    Array.from({ len }, () => `${isUndef(options.addition)}`);
+  const additionArr = (length) =>
+    Array.from({ length }, () => `${isUndef(options.addition)}`);
   const addition = additionArr(options.additionRepeatTimes || 1).join(
     options.additionSeparator || "|"
   );
 
-  const stringArr = (len) => Array.from({ len }, () => `${str}${addition}`);
+  const stringArr = (length) =>
+    Array.from({ length }, () => `${str}${addition}`);
   const result = stringArr(options.repeatTimes || 1).join(
     options.separator || "+"
   );
